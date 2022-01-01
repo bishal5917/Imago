f5f9909921d8acd7aef3c46d1b85461e86159dc8
Bishal Poudel <poudelb172@gmail.com>
2022-01-01 12:23:51 +0545

    firebase configured

--- imago/App.js
+++ imago/App.js
@@ -6,8 +6,29 @@ import Home from './components/Home';
 import First from './components/First';
 import Login from './components/Login';
 import Register from './components/Register';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 const Stack = createNativeStackNavigator();
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCGg6mQLnCRgnjIe9v9l7I0sHdDOhXad_U",
+  authDomain: "imago-29158.firebaseapp.com",
+  projectId: "imago-29158",
+  storageBucket: "imago-29158.appspot.com",
+  messagingSenderId: "824882338075",
+  appId: "1:824882338075:web:86d1f4e333506e15d31bbe",
+  measurementId: "G-M52G0RTE7E"
+};
+
+let app;
+
+if (firebase.apps.length === 0) {
+  app = firebase.initializeApp(firebaseConfig)
+} else {
+  app = firebase.app();
+}
 
 export default function App() {
   return (
